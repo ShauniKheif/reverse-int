9a98ad6654178477a01eb01dc0e47228dd580965
ShauniKheif <53867436+ShauniKheif@users.noreply.github.com>
2021-03-03 14:44:40 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+    n = String(n);
+    let arr = n.split('');
+    arr = arr.reverse();
+    arr = arr.join('');
+    return +arr;
 }
